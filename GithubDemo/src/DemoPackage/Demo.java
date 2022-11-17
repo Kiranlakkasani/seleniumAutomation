3b9e1cbaa20e1a1f642c1efa190c03ef49b70ef9
Soujanya <Soujanya@sowjanya>
2022-11-17 14:21:44 +0530

    first commit

---
+++ GithubDemo/src/DemoPackage/Demo.java
@@ -0,0 +1,11 @@
+package DemoPackage;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this isthe demo program ");
+		
+	}
+
+}
